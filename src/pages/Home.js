1531aeb7460a964f48d3c71b74b108aae4923969
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-03 22:30:13 +0700

    fix the reload on coin-list page in prod

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
-        <main>
-          
-        </main>
-        <Footer />
+      <Navbar />
+      <main className="container mx-auto lg:px-20 px-2">
+        <Link to="/coin-list">
+          <p className="p-10 underline text-sky-700 hover:text-sky-500">Check out the top 50 cryptocurrencies</p>
+        </Link>
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
